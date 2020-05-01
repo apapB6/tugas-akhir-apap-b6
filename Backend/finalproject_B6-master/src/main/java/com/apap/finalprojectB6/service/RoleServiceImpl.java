a34017a43458097f2897f8d80a5a8e45d03f8709
Riska Kurnia Dewi <riska.kurnia81@ui.ac.id>
2020-05-01 21:04:33 +0700

    first commit

---
+++ Backend/finalproject_B6-master/src/main/java/com/apap/finalprojectB6/service/RoleServiceImpl.java
@@ -0,0 +1,28 @@
+package com.apap.finalprojectB6.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.apap.finalprojectB6.model.RoleModel;
+import com.apap.finalprojectB6.repository.RoleDB;
+
+
+@Service
+@Transactional
+public class RoleServiceImpl implements RoleService {
+	@Autowired
+	private RoleDB roledb;
+
+	@Override
+	public RoleModel getRoleByNama(String nama) {
+		return roledb.findByNama(nama);
+	}
+	
+	@Override
+	public RoleModel getRoleById(int id) {
+		return roledb.findById(id);
+	}
+}
