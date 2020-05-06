d570654133d4e88ca6ff14f3c03e3cee5bc7023f
dwianaambarr <dwianaambarr@gmail.com>
2020-05-06 11:58:56 +0700

    remove frontend

--- Frontend/si-perpustakaan-frontend-master/src/views/Dashboard/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as TotalPengadaan } from './TotalPengadaan';
-export { default as LatestOrders } from './LatestOrders';
-export { default as TotalBuku } from './TotalBuku';
-export { default as TotalUsers } from './TotalUsers';
