07b59f0fe91f2b13e2b46120a99be35aa511f5f7
dwianaambarr <dwianaambarr@gmail.com>
2020-05-17 21:10:22 +0700

    user updated

--- src/main/java/com/apap/finalprojectB6/controller/WebserviceController.java
+++ src/main/java/com/apap/finalprojectB6/controller/WebserviceController.java
@@ -22,15 +22,16 @@ import org.springframework.web.client.RestTemplate;
 
 import com.apap.finalprojectB6.model.RoleModel;
 import com.apap.finalprojectB6.model.UserModel;
+import com.apap.finalprojectB6.model.UserWebServiceModel;
 import com.apap.finalprojectB6.service.RoleService;
 import com.apap.finalprojectB6.service.UserService;
 
 //
 //
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/perpustakaan")
 @CrossOrigin(origins = "*")
-public class ApiController {
+public class WebserviceController {
 	@Autowired
 	private UserService userService;
 
@@ -38,14 +39,20 @@ public class ApiController {
 	private RoleService roleService;
 
 	
-	@GetMapping(value = "/getuser")
-	public String getAllUser(Model model) 
-	{
-		String url = "https://webservice-situ.free.beeceptor.com/perpustakaan/user/viewall";
-	    RestTemplate restTemplate = new RestTemplate();
-	    String result = restTemplate.getForObject(url, String.class);
-	     
-	    return result;
+//	@GetMapping(value = "/getuser")
+//	public String getAllUser(Model model) 
+//	{
+//		String url = "https://webservice-situ.free.beeceptor.com/perpustakaan/user/viewall";
+//	    RestTemplate restTemplate = new RestTemplate();
+//	    String result = restTemplate.getForObject(url, String.class);
+//	     
+//	    return result;
+//	}
+	
+	@GetMapping(value = "/getAllUser")
+	private List<UserWebServiceModel> pengguna(Model model) {
+		List<UserWebServiceModel> user = userService.getUserPerpustakaan();
+		return user;
 	}
 
 }
