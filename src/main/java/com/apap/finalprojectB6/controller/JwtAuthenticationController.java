dfa9717ca65d62bc0e2e69824a4c4b62774717cc
Riska Kurnia Dewi <riska.kurnia81@ui.ac.id>
2020-05-23 00:07:27 +0700

    comment jwt

--- src/main/java/com/apap/finalprojectB6/controller/JwtAuthenticationController.java
+++ src/main/java/com/apap/finalprojectB6/controller/JwtAuthenticationController.java
@@ -1,60 +1,60 @@
-package com.apap.finalprojectB6.controller;
+// package com.apap.finalprojectB6.controller;
 
-import com.apap.finalprojectB6.model.JwtRequestModel;
-import com.apap.finalprojectB6.model.JwtResponseModel;
-import com.apap.finalprojectB6.model.UserModel;
-import com.apap.finalprojectB6.repository.UserRoleDB;
-import com.apap.finalprojectB6.security.JwtTokenUtil;
-import com.apap.finalprojectB6.service.JwtUserDetailsService;
+// import com.apap.finalprojectB6.model.JwtRequestModel;
+// import com.apap.finalprojectB6.model.JwtResponseModel;
+// import com.apap.finalprojectB6.model.UserModel;
+// import com.apap.finalprojectB6.repository.UserRoleDB;
+// import com.apap.finalprojectB6.security.JwtTokenUtil;
+// import com.apap.finalprojectB6.service.JwtUserDetailsService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.security.authentication.AuthenticationManager;
+// import org.springframework.security.authentication.BadCredentialsException;
+// import org.springframework.security.authentication.DisabledException;
+// import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@CrossOrigin
-public class JwtAuthenticationController {
+// @RestController
+// @CrossOrigin
+// public class JwtAuthenticationController {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+//     @Autowired
+//     private AuthenticationManager authenticationManager;
 
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
+//     @Autowired
+//     private JwtTokenUtil jwtTokenUtil;
 
-    @Autowired
-    private JwtUserDetailsService userDetailsService;
+//     @Autowired
+//     private JwtUserDetailsService userDetailsService;
 
-    @Autowired
-    private UserRoleDB userDb;
+//     @Autowired
+//     private UserRoleDB userDb;
 
-    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequestModel authenticationRequest) throws Exception{
-        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+//     @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+//     public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequestModel authenticationRequest) throws Exception{
+//         authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
 
-        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+//         final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
 
-        final String token = jwtTokenUtil.generateToken(userDetails);
-        UserModel userAuth = userDb.findByUsername(authenticationRequest.getUsername());
+//         final String token = jwtTokenUtil.generateToken(userDetails);
+//         UserModel userAuth = userDb.findByUsername(authenticationRequest.getUsername());
 
-        return ResponseEntity.ok(new JwtResponseModel(token, userAuth.getNama(), userAuth.getUsername(), userAuth.getUuid(), userAuth.getId_role()));
-    }
+//         return ResponseEntity.ok(new JwtResponseModel(token, userAuth.getNama(), userAuth.getUsername(), userAuth.getUuid(), userAuth.getId_role()));
+//     }
 
-    private void authenticate(String username, String password) throws Exception {
-        try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-        } catch (DisabledException e) {
-            throw new Exception("USER_DISABLED", e);
-        } catch (BadCredentialsException e) {
-            throw new Exception("INVALID_CREDENTIALS", e);
-        }
-    }
-}
+//     private void authenticate(String username, String password) throws Exception {
+//         try {
+//             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+//         } catch (DisabledException e) {
+//             throw new Exception("USER_DISABLED", e);
+//         } catch (BadCredentialsException e) {
+//             throw new Exception("INVALID_CREDENTIALS", e);
+//         }
+//     }
+// }
--- src/main/java/com/apap/finalprojectB6/service/JwtUserDetailsService.java
+++ src/main/java/com/apap/finalprojectB6/service/JwtUserDetailsService.java
@@ -1,8 +1,8 @@
-package com.apap.finalprojectB6.service;
+// package com.apap.finalprojectB6.service;
 
-import org.springframework.security.core.userdetails.UserDetails;
-import java.util.List;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import java.util.List;
 
-public interface JwtUserDetailsService {
-    UserDetails loadUserByUsername(String username);
-}
+// public interface JwtUserDetailsService {
+//     UserDetails loadUserByUsername(String username);
+// }
