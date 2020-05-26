bb9834b45c611ea17ba63b86dc2ae84099ef7dff
Riska Kurnia Dewi <riska.kurnia81@ui.ac.id>
2020-05-26 22:01:53 +0700

    adding jwt

--- src/main/java/com/apap/finalprojectB6/controller/RootController.java
+++ src/main/java/com/apap/finalprojectB6/controller/RootController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RootController {
     @RequestMapping("/check")
     private String home() {
-        return "check";
+        return "home";
     }
 }
