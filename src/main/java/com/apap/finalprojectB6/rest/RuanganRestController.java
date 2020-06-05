8f073b6fa4ac3936e50506748a9caa4ea2cde48a
dwianaambarr <dwianaambarr@gmail.com>
2020-06-05 22:47:33 +0700

    fixing

--- src/main/java/com/apap/finalprojectB6/rest/RuanganRestController.java
+++ src/main/java/com/apap/finalprojectB6/rest/RuanganRestController.java
@@ -38,17 +38,14 @@ public class RuanganRestController {
 	private PengadaanModel addSubmit(@RequestBody PengadaanModel pengadaan){
 		UserModel user = userService.getUserByUuid(pengadaan.getUuid_user());
 		
-		String[] cekUser = koperasi.getAllUser().split("[,{}: \n]");
-		System.out.println(koperasi.getAllUser());
+		//String[] cekUser = koperasi.getAllUser().split("[,{}: \n]");
 		if(user.getId_role() == 5) {
 			pengadaan.setStatus(1);		
 		}else if (user.getId_role() == 3){
-			for(int i = 0; i<cekUser.length; i++) {
-				System.out.println(cekUser[i]);
-				if(user.getUsername().equals(cekUser[i])) {
-					System.out.println(cekUser[i+3]);
-					int simpanan=Integer.parseInt(cekUser[i+6]);
-					if((cekUser[i+3].equals("6")) && (simpanan > 1000000)) {
+			for(int i = 0; i<koperasi.getAllUser().length(); i++) {
+				if(user.getUsername().equals(Character.toString(koperasi.getAllUser().charAt(i)))) {
+					int simpanan=Integer.parseInt(Character.toString(koperasi.getAllUser().charAt(i+4)));
+					if((Character.toString(koperasi.getAllUser().charAt(i+2)).equals("6")) && (simpanan > 1000000)) {
 						pengadaan.setStatus(3);
 					}else {
 						break;
